fe84c4cb588376e83e66f5c20396be32d7c1469a
Zhenggen Xu <zxu@linkedin.com>
2019-04-23 23:04:39 -0700

    upgrade to 3.5.1.2

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.5.1.1_amd64.deb
-$(BRCM_SAI)_URL = "http://172.25.11.11/lnos/sonic/Xu/SAI/libsaibcm_3.5.1.1_amd64.deb"
+BRCM_SAI = libsaibcm_3.5.1.2_amd64.deb
+$(BRCM_SAI)_URL = "http://172.25.11.11/lnos/sonic/Xu/SAI/libsaibcm_3.5.1.2_amd64.deb"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.5.1.1_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.5.1.2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "http://172.25.11.11/lnos/sonic/Xu/SAI/libsaibcm-dev_3.5.1.1_amd64.deb"
+$(BRCM_SAI_DEV)_URL = "http://172.25.11.11/lnos/sonic/Xu/SAI/libsaibcm-dev_3.5.1.2_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
