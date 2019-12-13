1ba309076ad8b5c3ea777d5113a1ebe46977f4d4
Sangita Maity <sangitamaity0211@gmail.com>
2019-12-13 10:32:20 -0800

    Click Upgrade to 7.0.0 (#20)
    Signed-off-by: Sangita Maity <sangitamaity0211@gmail.com>

--- slave.mk
+++ slave.mk
@@ -613,7 +613,6 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_INSTALLERS)) : $(TARGET_PATH)/% : \
         $(addprefix $(STRETCH_DEBS_PATH)/,$(INITRAMFS_TOOLS) \
                 $(LINUX_KERNEL) \
                 $(SONIC_DEVICE_DATA) \
-                $(PYTHON_CLICK) \
                 $(IFUPDOWN2) \
                 $(KDUMP_TOOLS) \
                 $(LIBPAM_TACPLUS) \
