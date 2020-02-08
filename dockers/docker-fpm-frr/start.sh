b26c5fdc40ec38cde2ba7dbca8c377b80a49fa74
Zhenggen Xu <zxu@linkedin.com>
2020-02-08 05:53:21 +0000

    [docker-frr] Fix the start.sh where it fails in case no WARM_RESTART in configDB
    Signed-off-by: Zhenggen Xu <zxu@linkedin.com>

--- dockers/docker-fpm-frr/start.sh
+++ dockers/docker-fpm-frr/start.sh
@@ -34,7 +34,7 @@ rm -f /var/run/rsyslogd.pid
 supervisorctl start rsyslogd
 
 # start eoiu pulling, only if configured so
-if [[ $(sonic-cfggen -d -v 'WARM_RESTART.bgp.bgp_eoiu') == 'true' ]]; then
+if [[ ! -z $(sonic-cfggen -d -v 'WARM_RESTART') ]] && [[ $(sonic-cfggen -d -v 'WARM_RESTART.bgp.bgp_eoiu') == 'true' ]]; then
     supervisorctl start bgp_eoiu_marker
 fi
 
