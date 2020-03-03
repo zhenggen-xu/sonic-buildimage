7498d55014fcb86ef893cf0d9ebd8e949d9c4bad
Zhenggen Xu <zxu@linkedin.com>
2020-03-03 20:56:44 +0000

    Update SAI binary for hostif fix
    Signed-off-by: Zhenggen Xu <zxu@linkedin.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.7.4.1-LI1-test_amd64.deb
-$(BRCM_SAI)_URL = "http://172.21.47.11/lnos/sonic/Xu/SAI/libsaibcm_3.7.4.1-LI1-test_amd64.deb"
+BRCM_SAI = libsaibcm_3.7.4.1-LI2-test_amd64.deb
+$(BRCM_SAI)_URL = "http://172.21.47.11/lnos/sonic/Xu/SAI/libsaibcm_3.7.4.1-LI2-test_amd64.deb"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.7.4.1-LI1-test_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.7.4.1-LI2-test_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "http://172.21.47.11/lnos/sonic/Xu/SAI/libsaibcm-dev_3.7.4.1-LI1-test_amd64.deb"
+$(BRCM_SAI_DEV)_URL = "http://172.21.47.11/lnos/sonic/Xu/SAI/libsaibcm-dev_3.7.4.1-LI2-test_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
